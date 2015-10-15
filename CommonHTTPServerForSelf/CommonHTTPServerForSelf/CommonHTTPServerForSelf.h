a7dac330319c083b9b1250dc6a1646643b042561
mac <mac@dafenqimima1.local>
2015-10-15 11:47:20 +0800

    commint

---
+++ CommonHTTPServerForSelf/CommonHTTPServerForSelf/CommonHTTPServerForSelf.h
@@ -0,0 +1,19 @@
+//
+//  CommonHTTPServerForSelf.h
+//  CommonHTTPServerForSelf
+//
+//  Created by mac on 15/10/15.
+//  Copyright © 2015年 longyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonHTTPServerForSelf.
+FOUNDATION_EXPORT double CommonHTTPServerForSelfVersionNumber;
+
+//! Project version string for CommonHTTPServerForSelf.
+FOUNDATION_EXPORT const unsigned char CommonHTTPServerForSelfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonHTTPServerForSelf/PublicHeader.h>
+
+
---
+++ CommonHTTPServerForSelf/CommonHTTPServerForSelfTests/CommonHTTPServerForSelfTests.m
@@ -0,0 +1,39 @@
+//
+//  CommonHTTPServerForSelfTests.m
+//  CommonHTTPServerForSelfTests
+//
+//  Created by mac on 15/10/15.
+//  Copyright © 2015年 longyuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CommonHTTPServerForSelfTests : XCTestCase
+
+@end
+
+@implementation CommonHTTPServerForSelfTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
